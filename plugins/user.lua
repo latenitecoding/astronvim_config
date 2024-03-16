6620fcf247c877282eff95623a48aaeb49653873
latenitecoding <latenitecode@gmail.com>
2024-03-15 20:14:26 -0500

    moves from rust-tools to rustaceanvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -68,26 +68,10 @@ return {
       }
     end
   },
-  -- Rust
   {
-    "simrat39/rust-tools.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      local rt = require("rust-tools")
-      rt.setup({
-          server = {
-              on_attach = function(_, bufnr)
-                  -- Hover actions
-                  vim.keymap.set("n", "<C-l>", rt.hover_actions.hover_actions, { buffer = bufnr })
-                  -- Code action groups
-                  vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-              end,
-          },
-      })
-    end
+    "mrcjkb/rustaceanvim",
+    version = "^4", -- Recommended
+    ft = { "rust" },
   },
   -- Copilot
   {
