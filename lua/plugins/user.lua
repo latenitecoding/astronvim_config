bd83380157c6b9a631b00f21afa596c73b0b90dc
latenitecode <85068850+latenitecoding@users.noreply.github.com>
2025-05-01 14:06:16 -0500

    Update user.lua

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -17,7 +17,7 @@ return {
     config = function()
       require("lualine").setup {
         options = {
-          theme = "tokyonight"
+          theme = "rose-pine"
         }
       }
     end,
